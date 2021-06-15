837657974e7631b3d8a1b2ce77ee0a9c68a5bd45
gstephan30 <gstephan30@gmail.com>
2021-06-15 12:40:32 +0200

    added value box 4 serohub

--- generate.bat
+++ generate.bat
@@ -1,2 +1,3 @@
 Rscript.exe --encoding=UTF-8 "..\..\01_R scripts\generate_html_summary.R"
 Rscript.exe -e "rmarkdown::render('index.Rmd')"
+Rscript.exe valuebox.R
---
+++ valuebox.R
@@ -0,0 +1,33 @@
+library(dplyr)
+library(stringr)
+library(lubridate)
+library(ggplot2)
+
+rds_files <- list.files(path = "../data/", pattern = "_value.rds", full.names = TRUE) %>% 
+  as_tibble() %>% 
+  mutate(date = str_extract(value, "[0-9]{8}"),
+         date = ymd(date)) %>% 
+  arrange(desc(date)) %>% 
+  pull(value) %>% 
+  .[1]
+
+value <- readRDS(rds_files)
+
+valuebox <- tibble(x = 2,
+                   y = 1,
+                   text = paste0("Available Cases:\n", value),
+                   fill =  "#d23264") %>%
+  ggplot(aes(x, y, label = text, fill = fill)) +
+  geom_tile() +
+  geom_text(fontface = "bold", size = 12, color = "white") +
+  scale_fill_identity() +
+  theme_void() +
+  scale_x_continuous(expand=c(0,0)) +
+  scale_y_continuous(expand=c(0,0)) +
+  labs(x=NULL, y=NULL, title=NULL) 
+
+ggsave("valuebox.png",
+       plot = valuebox,
+       dpi = 300,
+       width = 4,
+       height = 2)
